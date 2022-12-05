81ff6e6d9cbe7caea456bd12ed77a24b46496e5a
EmmaRamRe <88679112+EmmaRamRe@users.noreply.github.com>
2022-12-05 14:41:18 -0600

    update: neew header

--- Front_End/components/Header.tsx
+++ Front_End/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import {AUTH_TOKEN } from '../constants';
 
 
 type Props = {};
 
 function Header({}: Props) {
+const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div className="sticky top-0 p-5 flex items-center justify-between max-w-7xl mx-auto z-20 shadow bg-teal-100 rounded-lg bg-opacity-70 backdrop-blur-sm">
       <div className="flex flex-row items-center">
@@ -20,7 +22,23 @@ function Header({}: Props) {
       </div>
       <div className="flex flex-row items-center">
         <div>
-          <Link href="/LoginContainer">Iniciar sesión</Link>
+        {authToken ?(
+          <div
+          onClick={() => {
+            localStorage.removeItem(AUTH_TOKEN);
+            <Link href="/LoginContainer">Iniciar sesión</Link>
+          }}
+          >
+            logout
+          </div>
+        ): (
+          <Link 
+          to="/login"
+          className="ml1 no-underline black"
+          >
+            login
+          </Link>
+        )}
         </div>
       </div>
     </div>
