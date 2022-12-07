2a43d6b6d35cf2067885b82d1c50e1399b9d7491
Nezih Nieto Gutiérrez <100229436+nezih-niegu@users.noreply.github.com>
2022-12-06 20:32:04 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 python3 -m pip install -r requirements.txt
 source proyecto_final-venv/bin/activate
 (trap 'kill 0' SIGINT; ./runBackend.sh & ./hackernews.sh & ./runFrontend.sh)
+killall -9 python3
 
