1c93e86ca8836413cf23d0979e336d56753ba807
Nezih Nieto Gutiérrez <100229436+nezih-niegu@users.noreply.github.com>
2022-12-07 19:49:12 -0600

    Update settings.py

--- backEnd/backEnd/settings.py
+++ backEnd/backEnd/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from decouple import config
+from pathlib import path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'health_check',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
