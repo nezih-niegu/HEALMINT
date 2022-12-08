1bca6a18973de0ada2cc74611bfd73f2641cef2b
Nezih Nieto Gutiérrez <100229436+nezih-niegu@users.noreply.github.com>
2022-12-07 20:16:56 -0600

    Update urls.py

--- backEnd/backEnd/urls.py
+++ backEnd/backEnd/urls.py
@@ -21,4 +21,5 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path(r'^ht/',include('health_check.urls')),
 ]
