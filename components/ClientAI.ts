82714d528c43cdb3967bc812849d9e68df7ad747
Nezih Nieto Gutiérrez <100229436+nezih-niegu@users.noreply.github.com>
2022-12-07 20:03:57 -0600

    Delete components directory

--- components/ClientAI.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-const Client = new ApolloClient({
-  uri: 'http://localhost:8001/graphql/',
-  cache: new InMemoryCache()
-});
-
-export default Client;
